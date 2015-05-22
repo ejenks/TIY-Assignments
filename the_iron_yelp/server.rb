c7f669faad48ab667dc92f7becb388d693db0bf3
Erik Jenks <erikjenks@gmail.com>
2015-05-22 10:58:39 -0400

    Updated server

--- the_iron_yelp/server.rb
+++ the_iron_yelp/server.rb
@@ -1,3 +1,22 @@
 require 'webrick'
 require 'erb'
 require 'json'
+
+server = WEBrick::HTTPServer.new(Port: 8000) 
+
+server.mount_proc "/" do |request, response|
+	response.body = File.read "home.html"
+end
+
+server.mount_proc "/shutdown" do |request, response|
+	response.body = "Thanks for coming and see you later!"
+	server.shutdown
+end
+
+
+
+
+
+
+
+server.start
