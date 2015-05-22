bf309b5d13e52f23d642df750540c085aa968755
Erik Jenks <erikjenks@gmail.com>
2015-05-22 12:19:52 -0400

    Updated server file

--- the_iron_yelp/restaurant.rb
+++ the_iron_yelp/restaurant.rb
@@ -1,6 +1,17 @@
 class Restaurant
 
-	def initialize
+	def initialize(id, name, address, website, hours, price, cuisine, rating)
+		@id = id
+		@name = name
+		@address = address
+		@website = website
+		@hours = hours
+		@price = price
+		@cuisine = cuisine
+		@rating = rating
+	end
+
+	def all
 		
 	end
 end
--- the_iron_yelp/server.rb
+++ the_iron_yelp/server.rb
@@ -1,6 +1,10 @@
 require 'webrick'
 require 'erb'
 require 'json'
+require './restaurant.rb'
+
+restaurants_file = File.read "restaurants.json"
+restaurants_array = JSON.parse restaurants_file
 
 server = WEBrick::HTTPServer.new(Port: 8000) 
 
@@ -8,15 +12,10 @@ server.mount_proc "/" do |request, response|
 	response.body = File.read "home.html"
 end
 
+
 server.mount_proc "/shutdown" do |request, response|
 	response.body = "Thanks for coming and see you later!"
 	server.shutdown
 end
-
-
-
-
-
-
 
 server.start
