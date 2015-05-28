6b0330d11f3498db481e630bbd5f39bf095f3afe
Erik Jenks <erikjenks@gmail.com>
2015-05-27 22:52:38 -0400

    Did more beast mode
    Still some issues with the rest of beast mode stuff.

--- active_record_cars/car.rb
+++ active_record_cars/car.rb
@@ -4,6 +4,11 @@ class Car < ActiveRecord::Base
 	establish_connection adapter: 'sqlite3',
 						 database: 'cars.db'
 
+	def luxury
+		if Car.price > 30
+		end
+	end
+
 	
 end
 
--- active_record_cars/server.rb
+++ active_record_cars/server.rb
@@ -1,22 +1,57 @@
 require "webrick"
 require "erb"
-require "./cartemplate.rb"
+require_relative "car.rb"
 
 server = WEBrick::HTTPServer.new(:Port => 8000)
 
 server.mount_proc "/" do |request, response|
-	response.body = File.read "home.html"
+	response.body = File.read "home.html.erb"
 end
 
-@pages = ["compact", "sporty", "large", "mazda", "pontiac", "luxury", "all"]
-@pages.each do |p|
-	server.mount_proc "/#{p}" do |request, response|
-		@cars = Car.stuff
-		@cars = @cars.select{|car| car.send("#{p}" + "?")}
-		template = ERB.new(File.read "template.html.erb")
-		response.body = template.result
-	end
-end	
+server.mount_proc "/ford" do |request, response|
+	@car = Car.where(make: "Ford")
+	template = ERB.new(File.read "template.html.erb")
+	response.body = template.result
+end
+
+server.mount_proc "/mazda" do |request, response|
+	@car = Car.where(make: "Mazda")
+	template = ERB.new(File.read "template.html.erb")
+	response.body = template.result
+end
+
+server.mount_proc "/sporty" do |request, response|
+	@car = Car.where(category: "Sporty")
+	template = ERB.new(File.read "template.html.erb")
+	response.body = template.result
+end
+
+server.mount_proc "/luxury" do |request, response|  ### this doesnt work
+	@car = Car.luxury
+	template = ERB.new(File.read "template.html.erb") 	
+	response.body = template.result
+end
+
+# server.mount_proc "/fuel_efficient" do |request, response| ### this doesnt work
+#  	@car = Car.where(mpg_highway: > 30)
+#  	template = ERB.new(File.read "template.html.erb")
+#  	response.body = template.result
+#  end
+
+# server.mount_proc "/domestic" do |request, response|  ### this doesnt work
+#  	@car = Car.where(make: "Ford", "Buick", "Cadillac", "Chevrolet", "Chrysler", "Dodge",
+#  						   "Eagle", "Lincoln", "Mercury", "Oldsmobile", "Plymouth", 						   "Pontiac", "Saturn")
+#  	template = ERB.new(File.read "template.html.erb")
+#  	response.body = template.result
+#  end
+
+#  server.mount_proc "/foreign" do |request, response|  #### this doesnt work
+#  	@car = Car.where(make: "Acura", "Audi", "BMW", "Geo", "Honda", "Hyundai",
+#  						   "Lexus", "Mazda", "Mercedes-Benz", "Mitsubishi",
+#  						   "Nissan", "Saab", "Subaru", "Suzuki", "Toyota")
+#  	template = ERB.new(File.read "template.html.erb")
+#  	response.body = template.result
+#  end
 
 server.mount_proc "/shutdown" do |request, response|
 	response.body = "Good Bye and Thanks for Visiting!"
