e9cfc447961c9042ac94d918812827fdb1ca231d
Erik Jenks <erikjenks@gmail.com>
2015-05-27 21:53:00 -0400

    Black Bear
    had to add a .chomp method at the end of my array.

--- active_record_iron_roster/person.rb
+++ active_record_iron_roster/person.rb
@@ -4,7 +4,4 @@ class Person < ActiveRecord::Base
 	establish_connection adapter: 'sqlite3',
 						 database: 'iron_roster.db'
 
-	def to_s
-	end 
-
 end
--- active_record_iron_roster/setup.rb
+++ active_record_iron_roster/setup.rb
@@ -1,10 +1,23 @@
 require 'active_record'
+require './person.rb'
+
 ActiveRecord::Base.establish_connection adapter: 'sqlite3',
 										database: 'iron_roster.db'
 ActiveRecord::Base.connection.create_table 'people',
 	force: true do |table|
 		table.string :subject
-		table.string :github
+		table.string :github_account
 		table.string :name
 		table.boolean :student
+	end
+
+	student_strings = IO.readlines("iron_roster.csv")
+	student_strings.shift
+
+	student = student_strings.map do |student_string|
+		student_string.gsub!('"','')
+		student_array = student_string.split(",")
+
+		Person.create(subject: student_array[0], github_account: student_array[1], 
+					  name: student_array[2], student: student_array[3].chomp ) ### had to add the .chomp method otherwise it blew up with a deprecation error
 	end
